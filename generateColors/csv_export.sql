0dfd6ca1157cdfcabaf41602b72b4492f8937641
Younghoon Kim <yhoonkim01@gmail.com>
2016-05-31 14:26:56 -0700

    Removed wrong files (Sorry!)

--- generateColors/csv_export.sql
+++
@@ -1,9 +0,0 @@
-.headers on
-.mode csv
-.output user_answers_mturk.csv
-select * 
-from user_answers 
-where user_id in (39,40,41,42,43,44,45,46,47,48,49,50,53,56,57,58,59,60,61,62,63,64,65,66,67)
- and id <> 341;
-.output stdout
-.q
